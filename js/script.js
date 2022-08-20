b692fc8240e838c034a4924eab7e962e51f15109
Andrii Terokhin <andreyterokhin89@gmail.com>
2022-08-20 19:57:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use sctrict";
+
+// const obj = {
+//     name: "Andrii",
+//     age: 24,
+//     sex: "M"
+// };
+
+// console.log(obj.name, obj.age);
+
+// let question = confirm("Are you here?");
+
+// console.log(question);
+
+const answers = [];
+
+answers[0] = prompt("What is your name?", "");
+answers[1] = prompt("What old are you?", "");
+answers[2] = prompt("What is your favourite mark of car?", "");
+
+document.write(answers);
